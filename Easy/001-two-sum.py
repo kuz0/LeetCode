f8aafe7f2fca25a77695be9392b2ab9ba6e90d3f
kuz0 <kuz0@outlook.com>
2018-02-27 20:46:08 +0800

    001

---
+++ Easy/001-two-sum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def twoSum0(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        index = [x for x in range(len(nums))]
+        for i in index:
+            for j in index:
+                if i < j and nums[i] + nums[j] == target:
+                    return [i, j]
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [lookup[target - num], i]
+            lookup[num] = i
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum([2, 7, 11, 15], 22))
