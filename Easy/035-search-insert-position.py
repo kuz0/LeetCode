a08be5fbf76787de5b4dbf68ed515bb6172133a1
kuz0 <kuz0@outlook.com>
2018-03-05 18:52:19 +0800

    035

---
+++ Easy/035-search-insert-position.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
+
+
+if __name__ == "__main__":
+    print(Solution().searchInsert([1, 3, 5, 6], 5))
+    print(Solution().searchInsert([1, 3, 5, 6], 2))
+    print(Solution().searchInsert([1, 3, 5, 6], 7))
+    print(Solution().searchInsert([1, 3, 5, 6], 0))
