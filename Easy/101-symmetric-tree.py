bebe6c44421d23ed964f4c86a90c93a0e59bc535
kuz0 <kuz0@outlook.com>
2018-03-09 23:11:28 +0800

    101

---
+++ Easy/101-symmetric-tree.py
@@ -0,0 +1,42 @@
+# Definition for a  binary tree node
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    # @param root, a tree node
+    # @return a boolean
+    def isSymmetric(self, root):
+        if root is None:
+            return True
+        stack = []
+        stack.append(root.left)
+        stack.append(root.right)
+
+        while stack:
+            p, q = stack.pop(), stack.pop()
+
+            if p is None and q is None:
+                continue
+
+            if p is None or q is None or p.val != q.val:
+                return False
+
+            stack.append(p.left)
+            stack.append(q.right)
+
+            stack.append(p.right)
+            stack.append(q.left)
+
+        return True
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left, root.right = TreeNode(2), TreeNode(2)
+    root.left.left, root.right.right = TreeNode(3), TreeNode(3)
+    root.left.right, root.right.left = TreeNode(4), TreeNode(4)
+    print(Solution().isSymmetric(root))
