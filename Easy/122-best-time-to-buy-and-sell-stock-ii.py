44622b49deb4835fdd7e42dc2bb45cc4516702ea
kuz0 <kuz0@outlook.com>
2018-03-13 22:33:19 +0800

    122

---
+++ Easy/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(len(prices) - 1):
+            profit += max(0, prices[i + 1] - prices[i])
+        return profit
+
+
+if __name__ == "__main__":
+    result = Solution().maxProfit([3, 2, 1, 4, 2, 5, 6])
+    print(result)
