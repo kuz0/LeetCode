e6ea242ca2a9637012c9fa646ab22b1b5f962472
kuz0 <kuz0@outllok.com>
2018-03-28 13:36:10 +0800

    205

---
+++ Easy/205.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t))
+
+    def isIsomorphic1(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        hash_map = {}
+        map_val = set()
+        for i in range(len(s)):
+            if s[i] in hash_map:
+                if hash_map[s[i]] != t[i]:
+                    return False
+            elif t[i] in map_val:
+                return False
+            else:
+                hash_map[s[i]] = t[i]
+                map_val.add(t[i])
+        return True
