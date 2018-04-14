058294b2c459dfd5a5cbc3957f817abacdb4a7ae
kuz0 <kuz0@outlook.com>
2018-04-14 20:47:52 +0800

    update

---
+++ Array/pascals-triangle-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        result = [1]
+        for i in range(1, rowIndex + 1):
+            result = [1] + [result[j - 1] + result[j] for j in range(1, i)] + [1]
+        return result
